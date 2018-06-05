93d784ee1b61e9ed0494056c6a8286eee3e1afa4
Hector Bernabe <hector.bernabehernandez@schwab.com>
2018-06-05 10:29:32 -0600

    working on concourse

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -0,0 +1,11 @@
+package io.pivotal.pal.tracker;
+
+public class TimeEntry {
+
+    private long id;
+    private long projectId;
+    private long userId;
+    private LocalDate date;
+    private int hours;
+
+}
